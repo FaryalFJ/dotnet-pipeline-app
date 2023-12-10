03c88295f6471d22fc653d02b78e471bbf25b9b0
FaryalFJ <faryal.javed@gmail.com>
2023-12-11 00:40:17 +0500

    Initial commit

---
+++ MyDotNetApp/MyDotNetApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace MyDotNetApp.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void TestSample()
+        {
+            Assert.True(true);
+        }
+    }
+}
+
---
+++ MyDotNetApp/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyDotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
